7de267af297947ee99b47847b0c9467721422426
ssamo <ssamo@SAMOTRA>
2024-02-14 14:07:44 +0700

    Add project files.

---
+++ CookBook/ViewModels/MainViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CookBook.Models;
+
+namespace CookBook.ViewModels
+{
+    public partial class MainViewModel:ObservableObject
+    {
+        [ObservableProperty]
+        private string title="";
+
+        private CuisineList cuisineList = new CuisineList();
+
+        [ObservableProperty]
+        private ObservableCollection<Cuisine> cuisines;
+
+        [ObservableProperty]
+        private Cuisine selectedCuisine;
+        public MainViewModel()
+        {
+            Title = "CookBook";
+            cuisines = new ObservableCollection<Cuisine>(cuisineList.Cuisines);
+            SelectedCuisine = selectedCuisine;
+        }
+    }
+}
